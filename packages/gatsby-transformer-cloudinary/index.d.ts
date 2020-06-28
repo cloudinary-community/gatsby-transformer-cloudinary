6d00affe122bd1189913dcfab95cf3315646d0e8
Kurt Tomlinson <kurttomlinson@gmail.com>
2020-06-28 10:08:30 -0400

    add some type information

--- packages/gatsby-transformer-cloudinary/index.d.ts
+++ packages/gatsby-transformer-cloudinary/index.d.ts
@@ -1,1 +1,31 @@
-export declare function createRemoteImageNode(opts: any): any;
+import { NodePluginArgs, Store, GatsbyCache, Reporter } from 'gatsby';
+
+export function createRemoteImageNode(
+  args: CreateRemoteImageNodeArgs,
+): Promise<CloudinaryAssetNode>;
+
+export interface CreateRemoteImageNodeArgs {
+  url: string;
+  // store: Store;
+  // cache: GatsbyCache;
+  createNode: NodePluginArgs['actions']['createNode'];
+  createNodeId: NodePluginArgs['createNodeId'];
+  // auth?: {
+  //   htaccess_user: string;
+  //   htaccess_pass: string;
+  // };
+  // httpHeaders?: object;
+  // ext?: string;
+  // name?: string;
+  // reporter: Reporter;
+
+
+  //these were added
+  parentNodeId?: string;
+  relationshipName?: string;
+  parentNode?: any;
+  createContentDigest: NodePluginArgs['createContentDigest'];
+
+}
+
+export type CloudinaryAssetNode = any;
