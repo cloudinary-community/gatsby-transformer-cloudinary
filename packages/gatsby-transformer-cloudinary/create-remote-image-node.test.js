dca5ea3ea5fa82ccb36f05be8e523b9df97e0119
Kurt Tomlinson <kurttomlinson@gmail.com>
2020-07-01 08:54:53 -0400

    add more unit tests

--- packages/gatsby-transformer-cloudinary/create-remote-image-node.test.js
+++ packages/gatsby-transformer-cloudinary/create-remote-image-node.test.js
@@ -1,31 +1,12 @@
 const { createRemoteImageNode } = require('./create-remote-image-node');
 
-jest.mock('./upload', () => {
-  return {
-    uploadImageToCloudinary: jest.fn(() => {
-      const cloudinaryUploadResult = {
-        version: 123456789,
-        public_id: 'public_id',
-        height: 1080,
-        width: 1920,
-        responsive_breakpoints: [
-          { breakpoints: [{ width: 1920 }, { width: 960 }, { width: 480 }] },
-        ],
-      };
-      return cloudinaryUploadResult;
-    }),
-  };
-});
+jest.mock('./create-image-node');
+jest.mock('./options');
+jest.mock('./upload');
 
-jest.mock('./options', () => {
-  return {
-    getPluginOptions: jest.fn(() => {
-      return {
-        cloudName: 'cloudName',
-      };
-    }),
-  };
-});
+const { createImageNode } = require('./create-image-node');
+const { getPluginOptions } = require('./options');
+const { uploadImageToCloudinary } = require('./upload');
 
 function getDefaultArgs(args) {
   return {
@@ -35,6 +16,7 @@ function getDefaultArgs(args) {
     createNodeId: jest.fn(() => 'createNodeId'),
     createContentDigest: jest.fn(() => 'createContentDigest'),
     parentNode: { id: 'abc-123' },
+    overwriteExisting: false,
     ...args,
   };
 }
@@ -48,47 +30,86 @@ test('requires relationshipName', async () => {
   );
 });
 
-test('creates an imageNode object', async () => {
+test('calls uploadImageToCloudinary with overwrite from plugin options by default', async () => {
   const args = getDefaultArgs();
+  delete args.overwriteExisting;
 
-  expect(await createRemoteImageNode(args)).toMatchSnapshot();
+  const optionOverwrite = 'optionOverwrite';
+  getPluginOptions.mockReturnValue({ overwriteExisting: optionOverwrite });
+  createImageNode.mockReturnValue({ id: 'image-node-id' });
+
+  await createRemoteImageNode(args);
+
+  const expectedArgs = { overwrite: optionOverwrite };
+  expect(uploadImageToCloudinary).toHaveBeenCalledWith(
+    expect.objectContaining(expectedArgs),
+  );
+});
+
+test('calls uploadImageToCloudinary with overwrite from args if provided', async () => {
+  const argsOverwrite = 'argsOverwrite';
+  const args = getDefaultArgs({ overwriteExisting: argsOverwrite });
+
+  const optionOverwrite = 'optionOverwrite';
+  getPluginOptions.mockReturnValue({ overwriteExisting: optionOverwrite });
+  createImageNode.mockReturnValue({ id: 'image-node-id' });
+
+  await createRemoteImageNode(args);
+
+  const expectedArgs = { overwrite: argsOverwrite };
+  expect(uploadImageToCloudinary).toHaveBeenCalledWith(
+    expect.objectContaining(expectedArgs),
+  );
+});
+
+test('calls uploadImageToCloudinary with the correct arguments', async () => {
+  expect(0).toEqual(1);
+});
+
+test('passes the correct arguments to createImageNode', async () => {
+  const args = getDefaultArgs();
+  createImageNode.mockReturnValue({ id: 'image-node-id' });
+  const uploadImageToCloudinaryResult = 'uploadImageToCloudinaryResult';
+  uploadImageToCloudinary.mockReturnValue(uploadImageToCloudinaryResult);
+
+  await createRemoteImageNode(args);
+
+  const expectedArgs = {
+    relationshipName: args.relationshipName,
+    cloudinaryUploadResult: uploadImageToCloudinaryResult,
+    parentNode: args.parentNode,
+    createContentDigest: args.createContentDigest,
+    createNode: args.createNode,
+    createNodeId: args.createNodeId,
+  };
+  expect(createImageNode).toHaveBeenCalledWith(
+    expect.objectContaining(expectedArgs),
+  );
 });
 
 test("creates an imageNode in Gatsby's GraphQL layer", async () => {
   const createNode = jest.fn();
   const args = getDefaultArgs({ createNode });
+  const createImageNodeResult = 'createImageNodeResult';
+  createImageNode.mockReturnValue(createImageNodeResult);
 
   await createRemoteImageNode(args);
-  expect(createNode).toHaveBeenCalledWith(
-    {
-      breakpoints: [1920, 960, 480],
-      cloudName: 'cloudName',
-      id: 'createNodeId',
-      internal: {
-        contentDigest: 'createContentDigest',
-        type: 'CloudinaryAsset',
-      },
-      originalHeight: 1080,
-      originalWidth: 1920,
-      parent: 'abc-123',
-      public_id: 'public_id',
-      version: 123456789,
-    },
-    { name: 'gatsby-transformer-cloudinary' },
-  );
+  expect(createNode).toHaveBeenCalledWith(createImageNodeResult, {
+    name: 'gatsby-transformer-cloudinary',
+  });
 });
 
 test('links the newly created node to the provided parent node in GraphQL', async () => {
-  const parentNode = {};
+  const args = getDefaultArgs();
   const imageNodeId = 'image-node-id';
-  const createNodeId = jest.fn(() => imageNodeId);
-  const relationshipName = 'customRelationship';
-  const args = getDefaultArgs({ parentNode, createNodeId, relationshipName });
+  createImageNode.mockReturnValue({ id: imageNodeId });
   await createRemoteImageNode(args);
 
-  expect(parentNode[`${relationshipName}___NODE`]).toEqual(imageNodeId);
+  expect(args.parentNode[`${args.relationshipName}___NODE`]).toEqual(
+    imageNodeId,
+  );
 });
 
-test('overwrite', async () => {
+test('returns the image node that it created', async () => {
   expect(0).toEqual(1);
 });
