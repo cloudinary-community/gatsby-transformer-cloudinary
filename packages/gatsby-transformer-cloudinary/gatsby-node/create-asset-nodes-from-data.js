eac6584a4f7ce6d4cc9ad2d6f4c598e388110935
Kurt Tomlinson <kurttomlinson@gmail.com>
2020-07-26 19:24:04 -0400

    remove errors for missing parameters since createCloudinaryAssetNode is internal now

--- packages/gatsby-transformer-cloudinary/gatsby-node/create-asset-nodes-from-data.js
+++ packages/gatsby-transformer-cloudinary/gatsby-node/create-asset-nodes-from-data.js
@@ -5,7 +5,6 @@ exports.createAssetNodesFromData = async ({
   actions: { createNode },
   createNodeId,
   createContentDigest,
-  reporter,
 }) => {
   const assetDataKeys = getAssetDataKeys(node);
 
@@ -19,7 +18,6 @@ exports.createAssetNodesFromData = async ({
       createNodeId,
       parentNode: node,
       relationshipName: assetDataKey,
-      reporter,
     });
   });
 };
@@ -44,52 +42,8 @@ function createCloudinaryAssetNode({
   createNodeId,
   parentNode,
   relationshipName,
-  reporter,
   version,
 }) {
-  if (!reporter) {
-    throw Error('`reporter` is a required argument.');
-  }
-  if (!cloudName) {
-    reporter.panic(
-      '`cloudName` is a required argument. Pass the name of the Cloudinary cloud where the image is stored.',
-    );
-  }
-  if (!createContentDigest) {
-    reporter.panic('`createContentDigest` is a required argument.');
-  }
-  if (!createNode) {
-    reporter.panic('`createNode` is a required argument.');
-  }
-  if (!createNodeId) {
-    reporter.panic('`createNodeId` is a required argument.');
-  }
-  if (!originalHeight) {
-    reporter.panic(
-      '`originalHeight` is a required argument. Pass the original height of the image stored on Cloudinary.',
-    );
-  }
-  if (!originalWidth) {
-    reporter.panic(
-      '`originalWidth` is a required argument. Pass the original width of the image stored on Cloudinary.',
-    );
-  }
-  if (!parentNode) {
-    reporter.panic(
-      '`parentNode` is a required argument. Pass the parent node that this Cloudinary image should be a child of.',
-    );
-  }
-  if (!publicId) {
-    reporter.panic(
-      '`publicId` is a required argument. Pass the Cloudinary public_id under which the image is stored.',
-    );
-  }
-  if (!relationshipName) {
-    reporter.panic(
-      '`relationshipName` is a required argument. Pass a name for how this image is related to its parent (e.g. "coverPhoto").',
-    );
-  }
-
   const cloudinaryUploadResult = {
     public_id: publicId,
     height: originalHeight,
