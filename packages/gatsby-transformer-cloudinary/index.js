3a72d9b0b7e0c0c654387c8bebc097d90e55816f
Kurt Tomlinson <kurttomlinson@gmail.com>
2020-07-25 23:22:15 -0400

    export the new createCloudinaryAssetNode function

--- packages/gatsby-transformer-cloudinary/index.js
+++ packages/gatsby-transformer-cloudinary/index.js
@@ -1,3 +1,4 @@
+exports.createCloudinaryAssetNode = require('./create-cloudinary-asset-node').createCloudinaryAssetNode;
+exports.createRemoteImageNode = require('./create-remote-image-node').createRemoteImageNode;
 exports.getFixedImageObject = require('./get-image-objects').getFixedImageObject;
 exports.getFluidImageObject = require('./get-image-objects').getFluidImageObject;
-exports.createRemoteImageNode = require('./create-remote-image-node').createRemoteImageNode;
