9ede15c30f1f1cc6a8525ced8bf2a6f1342de82b
Kurt Tomlinson <kurttomlinson@gmail.com>
2020-11-23 20:33:59 -0500

    allow width and height to both be specified for fixed queries

---
+++ packages/gatsby-transformer-cloudinary/get-image-objects/get-aspect-ratio.js
@@ -0,0 +1,16 @@
+// retrieve aspect ratio if in transformation else create aspect ratio values
+exports.getAspectRatio = (transformations, originalAspectRatio) => {
+  const arTransform = transformations.find(t => t.startsWith('ar_'));
+  if (!arTransform) {
+    return originalAspectRatio;
+  }
+
+  const newAspectRatio = arTransform.replace('ar_', '');
+  if (newAspectRatio.indexOf(':') === -1) {
+    return Number(newAspectRatio);
+  }
+
+  const [w, h] = newAspectRatio.split(':').map(Number);
+
+  return w / h;
+};
