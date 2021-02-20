bb09c79c5431a50e9dccfcd729b558453dc3863a
william <williamimoh@yahoo.com>
2021-02-20 15:36:25 +0400

    typings and fixes to manual APIs

---
+++ packages/gatsby-transformer-cloudinary/api/index.d.ts
@@ -0,0 +1,47 @@
+export function getFixedImageObject(
+  args: GetFixedImageObjectArgs,
+): Promise<FixedImageObject>;
+
+export function getFluidImageObject(
+  args: GetFluidImageObjectArgs,
+): Promise<FluidImageObject>;
+
+export interface GetFixedImageObjectArgs {
+  base64Transformations?: string[],
+  base64Width?: number,
+  chained?: string[],
+  cloudName: string,
+  defaultBase64?: string,
+  fieldsToSelect?: string[],
+  defaultTracedSVG?: string,
+  height?: number,
+  ignoreDefaultBase64?: boolean,
+  originalHeight: number,
+  originalWidth: number,
+  public_id: string,
+  reporter?: Record<string, any>,
+  transformations?: string[],
+  version?: boolean,
+  width?: number,
+}
+
+export interface FixedImageObject {
+  height: number,
+  src: string
+  srcSet: string,
+  tracedSVG: string,
+  width: number,
+  base64: string | undefined
+}
+
+export interface GetFluidImageObjectArgs extends Omit<GetFixedImageObjectArgs, "height" | "width">{
+  breakpoints?: number[],
+  maxWidth?: number,
+}
+
+export interface FluidImageObject extends Omit<FixedImageObject, "height" | "width">{
+  aspectRatio: number,
+  presentationWidth: number,
+  presentationHeight: number,
+  sizes: string,
+}
--- packages/gatsby-transformer-cloudinary/index.js
+++ packages/gatsby-transformer-cloudinary/index.js
@@ -1,3 +1,1 @@
 exports.createRemoteImageNode = require('./create-remote-image-node').createRemoteImageNode;
-exports.getFixedImageObject = require('./get-image-objects').getFixedImageObject;
-exports.getFluidImageObject = require('./get-image-objects').getFluidImageObject;
