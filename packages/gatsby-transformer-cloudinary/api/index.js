4dfe2b7d56ce8a238522dec31b67bd69b2a86069
william <williamimoh@yahoo.com>
2021-02-20 15:51:38 +0400

    quick fix

--- packages/gatsby-transformer-cloudinary/api/index.js
+++ packages/gatsby-transformer-cloudinary/api/index.js
@@ -1,3 +1,3 @@
 // Runtime APIs for fetching remote images on Cloudinary
-exports.getFixedImageObject = require('./get-image-objects').getFixedImageObject;
-exports.getFluidImageObject = require('./get-image-objects').getFluidImageObject;
+exports.getFixedImageObject = require('../get-image-objects').getFixedImageObject;
+exports.getFluidImageObject = require('../get-image-objects').getFluidImageObject;
