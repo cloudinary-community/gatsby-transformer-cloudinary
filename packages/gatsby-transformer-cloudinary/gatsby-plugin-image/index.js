ad7d4077e5e90a17c2cdd1a5543ca4d65200a41e
Benedicte Raae <raae@raae.codes>
2022-06-15 15:43:13 +0200

    Conditionally add resolver

--- packages/gatsby-transformer-cloudinary/gatsby-plugin-image/index.js
+++ packages/gatsby-transformer-cloudinary/gatsby-plugin-image/index.js
@@ -1,27 +1,36 @@
-const { getGatsbyImageResolver } = require('gatsby-plugin-image/graphql-utils');
-const { resolveCloudinaryAssetData } = require('./cloudinary-image-utils');
-const { CloudinaryPlaceholderType } = require('./types');
+exports.createGatsbyImageDataResolver = (gatsbyUtils) => {
+  const { createResolvers, reporter } = gatsbyUtils;
+  try {
+    const {
+      getGatsbyImageResolver,
+    } = require('gatsby-plugin-image/graphql-utils');
+    const { resolveCloudinaryAssetData } = require('./cloudinary-image-utils');
+    const { CloudinaryPlaceholderType } = require('./types');
 
-exports.createGatsbyImageDataResolver = ({ createResolvers }) => {
-  createResolvers({
-    CloudinaryAsset: {
-      // loadImageData is your custom resolver, defined in step 2
-      gatsbyImageData: getGatsbyImageResolver(resolveCloudinaryAssetData, {
-        gravity: 'String',
-        x: 'Float',
-        y: 'Float',
-        zoom: 'Float',
-        crop: 'String',
-        quality: {
-          type: 'String',
-          defaultValue: 'auto',
-        },
-        transformations: '[String]',
-        chained: '[String]',
-        placeholder: {
-          type: CloudinaryPlaceholderType,
-        },
-      }),
-    },
-  });
+    createResolvers({
+      CloudinaryAsset: {
+        // loadImageData is your custom resolver, defined in step 2
+        gatsbyImageData: getGatsbyImageResolver(resolveCloudinaryAssetData, {
+          gravity: 'String',
+          x: 'Float',
+          y: 'Float',
+          zoom: 'Float',
+          crop: 'String',
+          quality: {
+            type: 'String',
+            defaultValue: 'auto',
+          },
+          transformations: '[String]',
+          chained: '[String]',
+          placeholder: {
+            type: CloudinaryPlaceholderType,
+          },
+        }),
+      },
+    });
+  } catch (error) {
+    reporter.warn(
+      '[gatsby-transformer-cloudinary] Install and configure gatsby-plugin-image to use the new GatsbyImage component and gatsbyImageData resolver'
+    );
+  }
 };
