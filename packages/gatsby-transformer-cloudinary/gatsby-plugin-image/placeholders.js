9f7dc716e935dc8801c685f98cadc8a24266977a
Benedicte Raae <raae@raae.codes>
2022-06-15 15:23:45 +0200

    remove console logs

--- packages/gatsby-transformer-cloudinary/gatsby-plugin-image/placeholders.js
+++ packages/gatsby-transformer-cloudinary/gatsby-plugin-image/placeholders.js
@@ -8,7 +8,6 @@ exports.getBase64Image = async (url) => {
       .get(url, { responseType: 'arraybuffer' })
       .then(({ data }) => {
         const base64 = Buffer.from(data, 'binary').toString('base64');
-        console.log('>>>> base64', url);
         return `data:image/jpeg;base64,${base64}`;
       });
   }
@@ -18,7 +17,6 @@ exports.getBase64Image = async (url) => {
 exports.getSvgImage = async (url) => {
   if (!placeholderCache[url]) {
     placeholderCache[url] = axios.get(url).then(({ data }) => {
-      console.log('>>>> svg', url);
       return `data:image/svg+xml,${encodeURIComponent(data)}`;
     });
   }
