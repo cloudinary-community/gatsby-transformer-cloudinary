7b3ca2467006b721547ba3403c8590029688fa20
raae <raae@raae.codes>
2023-07-28 07:34:13 +0200

    feature: add resolver log level

---
+++ demo/src/pages/manual-tests/varid-data-no-logs.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+
+const VariedDataPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allVariedData {
+        nodes {
+          name
+          expected
+          gatsbyImageData(
+            height: 200
+            backgroundColor: "#BADA55"
+            logLevel: "error"
+          )
+        }
+      }
+    }
+  `);
+
+  return data.allVariedData.nodes.map((node, index) => {
+    const gatsbyImage = getImage(node);
+
+    return (
+      <>
+        <h2>{node.name}</h2>
+        <div>
+          <strong>Expected:</strong> {node.expected}
+        </div>
+        {gatsbyImage ? (
+          <GatsbyImage key={index} image={gatsbyImage} alt={node.name} />
+        ) : (
+          <div>No image for node with name: {node.name}</div>
+        )}
+      </>
+    );
+  });
+};
+
+export default VariedDataPage;
