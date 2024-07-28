35dc48a8d5507576e2a93904001c29118d1ab148
raae <queen@raae.codes>
2024-07-28 22:00:09 +0200

    fix: remove deprecation research

--- plugin/node-creation/types.js
+++ plugin/node-creation/types.js
@@ -1,7 +1,7 @@
 exports.CloudinaryAssetType = `
   type CloudinaryAsset implements Node {
     id: ID!
-    publicId: String! @deprecated(reason: "Use \`public_id\` instead")
+    publicId: String!
     cloudName: String!
     version: String
     originalWidth: Int
